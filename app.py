ea2477651e2364ad276f72f4d1b2c9d638f7424b
ajayraajj <163561489+ajayraajj@users.noreply.github.com>
2024-03-22 17:37:53 +0000

    first day 2 commit

--- app.py
+++ app.py
@@ -1,40 +1,16 @@
 from flask import Flask, render_template
-from db import engine
-
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
+# Define the database engine
+engine = create_engine("mysql+pymysql://sql6693232:yDntHLHaqy@sql6.freesqldatabase.com/sql6693232?charset=utf8mb4")
 
-@app.route("/Home")
-def HomePage():
-    return render_template("home.html")
-
-
-@app.route("/Products")
-def ProductPage():
-    return render_template("products.html")
-
-
-@app.route("/Employees")
-def EmployeePage():
-    employees = loadEmployeesFromDb()
-    return render_template("employees.html", Emp=employees)
-
-
-
-@app.route("/Feedback")
-def FeedbackPage():
-  
-      return render_template("feedback.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000, debug=True)
-
+# Function to load employees from the database
 def loadEmployeesFromDb():
   with engine.connect() as conn:
       result = conn.execute(
-          text("SELECT id, name, salary, department, gender,                       hire_date, address, email, phone, birth_date,                           EmployeePhoto FROM employees"))
+          text("SELECT id, name, salary, department, gender, hireDate, address, email, phone, birthDate, EmployeePhoto FROM employees"))
       employees = []
       for row in result:
           employee_dict = {
@@ -43,7 +19,7 @@ def loadEmployeesFromDb():
               "salary": float(row[2]),
               "department": row[3],
               "gender": row[4],
-              "hire_date": str(row[5]),
+              "hireDate": str(row[5]),
               "address": row[6],
               "email": row[7],
               "phone": row[8],
@@ -53,5 +29,27 @@ def loadEmployeesFromDb():
           employees.append(employee_dict)
       return employees
 
+# Route for the Home page
+@app.route("/Home")
+def HomePage():
+    return render_template("home.html")
 
+# Route for the Products page
+@app.route("/Products")
+def ProductPage():
+    return render_template("products.html")
 
+# Route for the Employees page
+@app.route("/Employees")
+def EmployeePage():
+    employees = loadEmployeesFromDb()
+    return render_template("employees.html", Emp=employees)
+
+# Route for the Feedback page
+@app.route("/Feedback")
+def FeedbackPage():
+    return render_template("feedback.html")
+
+# Run the Flask application
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
